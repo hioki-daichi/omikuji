64cd79b6207d0fa9504e9e30f25fe2e9160e3cbc
Daichi Hioki <hiokidaichi@gmail.com>
2018-09-11 00:49:28 +0900

    Randomize fortunes

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "大吉")
+	fmt.Fprint(w, drawFortune())
+}
+
+func drawFortune() string {
+	ss := []string{"大吉", "中吉", "小吉", "吉", "末吉", "凶", "大凶"}
+	return ss[rand.Intn(len(ss))]
 }
--- main_test.go
+++ main_test.go
@@ -2,32 +2,64 @@ package main
 
 import (
 	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestMain_handler(t *testing.T) {
+func TestMain_handler_StatusCode(t *testing.T) {
+	t.Parallel()
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	handler(w, r)
 	rw := w.Result()
 	defer rw.Body.Close()
 
-	expectedStatusCode := http.StatusOK
-	actualStatusCode := rw.StatusCode
-	if actualStatusCode != expectedStatusCode {
-		t.Errorf(`unexpected status code: expected: "%d" actual: "%d"`, expectedStatusCode, actualStatusCode)
+	expected := http.StatusOK
+	actual := rw.StatusCode
+	if actual != expected {
+		t.Errorf(`unexpected status code: expected: "%d" actual: "%d"`, expected, actual)
+	}
+}
+
+func TestMain_handler_ResponseBody(t *testing.T) {
+	cases := map[string]struct {
+		seed     int64
+		expected string
+	}{
+		"KYOU":     {seed: 0, expected: "凶"},
+		"DAIKYOU":  {seed: 1, expected: "大凶"},
+		"SUEKICHI": {seed: 2, expected: "末吉"},
+		"KICHI":    {seed: 3, expected: "吉"},
+		"CHUKICHI": {seed: 4, expected: "中吉"},
+		"SHOKICHI": {seed: 5, expected: "小吉"},
+		"DAICHIKI": {seed: 9, expected: "大吉"},
 	}
 
-	b, err := ioutil.ReadAll(rw.Body)
-	if err != nil {
-		t.Fatalf("err %s", err)
-	}
+	for n, c := range cases {
+		c := c
+		t.Run(n, func(t *testing.T) {
+			// Can not use t.Parallel() because of rand.Seed
+			rand.Seed(c.seed)
 
-	expectedResponseBody := "大吉"
-	actualResponseBody := string(b)
-	if actualResponseBody != expectedResponseBody {
-		t.Errorf(`unexpected response body: expected: "%s" actual: "%s"`, expectedResponseBody, actualResponseBody)
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest("GET", "/", nil)
+			handler(w, r)
+			rw := w.Result()
+			defer rw.Body.Close()
+
+			b, err := ioutil.ReadAll(rw.Body)
+			if err != nil {
+				t.Fatalf("err %s", err)
+			}
+
+			expected := c.expected
+			actual := string(b)
+			if actual != expected {
+				t.Errorf(`unexpected response body: expected: "%s" actual: "%s"`, expected, actual)
+			}
+		})
 	}
 }
